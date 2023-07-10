9e377f41caf2402d214274d2c5b540bb550c733d
Ganesh Raju Galla  <61533653+GaneshRajuGalla@users.noreply.github.com>
2023-07-10 23:59:25 +0530

    Add files via upload

---
+++ MultipleSheets/MultipleSheets/MultipleSheetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultipleSheetsApp.swift
+//  MultipleSheets
+//
+//  Created by Ganesh on 10/07/23.
+//
+
+import SwiftUI
+
+@main
+struct MultipleSheetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
